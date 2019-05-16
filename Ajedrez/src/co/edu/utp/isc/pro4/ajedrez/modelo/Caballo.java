ded5475b82edf63ecd3b6ba897001ba217fec960
nelsonesled <50710122+nelsonesled@users.noreply.github.com>
2019-05-16 10:38:02 -0500

    Add files via upload

---
+++ Ajedrez/src/co/edu/utp/isc/pro4/ajedrez/modelo/Caballo.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.utp.isc.pro4.ajedrez.modelo;
+
+/**
+ *
+ * @author utp
+ */
+public class Caballo extends Ficha {
+
+    public Caballo(Color color) {
+        super(color);
+    }
+
+    @Override
+    public void mover() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void comer() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
---
+++ Ajedrez/src/co/edu/utp/isc/pro4/ajedrez/modelo/Peon.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.utp.isc.pro4.ajedrez.modelo;
+
+/**
+ *
+ * @author utp
+ */
+public class Peon extends Ficha {
+
+    public Peon(Color color) {
+        super(color);
+    }
+
+    @Override
+    public void mover() {
+        //TODO: Mover como peon
+    }
+
+    @Override
+    public void comer() {
+        //TODO: Comer como peon
+    }
+}
